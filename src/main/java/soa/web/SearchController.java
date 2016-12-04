249a3b3ed9370b5e4940dbe6a1fef4957b5ed982
beapc18 <beapc_18@hotmail.com>
2016-12-04 13:33:49 +0100

    add searching with max:n restriction

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class SearchController {
@@ -23,6 +26,29 @@ public class SearchController {
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+        Map<String,Object> header = new HashMap<>();
+
+        //Only limits the search if q contains the substring max:X
+        if(q.contains("max:")){
+            String[] max = q.split("max:");
+            Integer count = Integer.parseInt(max[max.length-1]);
+            //Accept the search of a string that contains max:X and only limit the results
+            //using de last max:X of the string
+            if(max.length>2){
+                q="";
+                for(int i=0; i<max.length-1; i++){
+                    q += max[i];
+                    if(i<max.length-2){
+                        q += "max:";
+                    }
+                }
+            }else{
+                q=max[0];
+            }
+            header.put("CamelTwitterCount",count);
+        }
+        header.put("CamelTwitterKeywords",q);
+
+        return producerTemplate.requestBodyAndHeaders("direct:search", "", header);
     }
 }
